1bd2528d6643afeeb99660c89d2a4d7ab8831a01
natsume.takeshita <takeshita.natsume@plus-zero.co.jp>
2023-05-02 12:18:58 +0900

    made addtochart function

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
+
+app.use(express.urlencoded({extended: false}));
+
 // const mysql = require('mysql');
 
 // require('dotenv').config();
@@ -25,7 +28,18 @@ const omusubilist = [
   ['いくら',500,'期間限定',5],
 ];
 
-const cart = []
+const cart = [
+  ['しおむすび',100,0],
+  ['焼きおにぎり',120,0],
+  ['梅干し',150,0],
+  ['昆布',150,0],
+  ['鮭',200,0],
+  ['ツナマヨ',200,0],
+  ['たらこ',200,0],
+  ['天むす',250,0],
+  ['スパむすび',250,0],
+  ['いくら',500,0],
+]
 
 // connection.connect((err) => {
 //   if (err) {
@@ -46,8 +60,29 @@ app.get('/list', (req, res) => {
   res.render('list.ejs',{omusubilist:omusubilist});
 });
 
+function addtocart(omusubi){
+
+  for (let i = 0;i<10;i++){
+    if (omusubi === cart[i][0]){
+      if (omusubilist[i][3]>0){
+        cart[i][2] += 1;
+        omusubilist[i][3]-=1; 
+      }
+    }
+  }
+ 
+}
+
 app.get('/cart', (req, res) => {
   res.render('cart.ejs');
+});
+
+app.post('/list', (req, res) => {
+  const omusubi = req.body.buyingOmusubi;
+  console.log(omusubi)
+  addtocart(omusubi);
+  console.log(cart)
+  res.redirect('/list');
 });
 
 
