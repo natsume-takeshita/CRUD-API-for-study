2ea86e3e5725aa6256c9d16d976ebb5eb92e32f6
natsume.takeshita <takeshita.natsume@plus-zero.co.jp>
2023-05-08 11:41:00 +0900

    use mysql

--- app.js
+++ app.js
@@ -4,42 +4,43 @@ const ejs = require('ejs');
 
 app.use(express.urlencoded({extended: false}));
 
-// const mysql = require('mysql');
+const mysql = require('mysql2');
+require('dotenv').config();
 
-// require('dotenv').config();
+const connection = mysql.createConnection({
+  host: process.env.DB_HOSTNAME,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+});
 
-// const connection = mysql.createConnection({
-//   host: process.env.DB_HOSTNAME,
-//   user: process.env.DB_USERNAME,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_DATABASE
-// });
+connection.connect();
 
-const omusubilist = [
-  ['しおむすび',100,'定番',10],
-  ['焼きおにぎり',120,'定番',10],
-  ['梅干し',150,'定番',10],
-  ['昆布',150,'定番',10],
-  ['鮭',200,'定番',10],
-  ['ツナマヨ',200,'定番',10],
-  ['たらこ',200,'定番',10],
-  ['天むす',250,'期間限定',5],
-  ['スパむすび',250,'期間限定',5],
-  ['いくら',500,'期間限定',5],
-];
+// const omusubilist = [
+//   ['しおむすび',100,'定番',10],
+//   ['焼きおにぎり',120,'定番',10],
+//   ['梅干し',150,'定番',10],
+//   ['昆布',150,'定番',10],
+//   ['鮭',200,'定番',10],
+//   ['ツナマヨ',200,'定番',10],
+//   ['たらこ',200,'定番',10],
+//   ['天むす',250,'期間限定',5],
+//   ['スパむすび',250,'期間限定',5],
+//   ['いくら',500,'期間限定',5],
+// ];
 
-const cart = [
-  ['しおむすび',100,0],
-  ['焼きおにぎり',120,0],
-  ['梅干し',150,0],
-  ['昆布',150,0],
-  ['鮭',200,0],
-  ['ツナマヨ',200,0],
-  ['たらこ',200,0],
-  ['天むす',250,0],
-  ['スパむすび',250,0],
-  ['いくら',500,0],
-]
+// const cart = [
+//   ['しおむすび',100,0],
+//   ['焼きおにぎり',120,0],
+//   ['梅干し',150,0],
+//   ['昆布',150,0],
+//   ['鮭',200,0],
+//   ['ツナマヨ',200,0],
+//   ['たらこ',200,0],
+//   ['天むす',250,0],
+//   ['スパむすび',250,0],
+//   ['いくら',500,0],
+// ]
 
 // connection.connect((err) => {
 //   if (err) {
@@ -57,48 +58,84 @@ app.get('/', (req, res) => {
 });
 
 app.get('/list', (req, res) => {
-  res.render('list.ejs',{omusubilist:omusubilist});
+  connection.execute(
+    'SELECT * FROM omusubilist',
+    [],
+    (error, results) => {
+      res.render('list.ejs', { omusubilist: results });
+    } 
+  )
 });
 
 app.get('/cart', (req, res) => {
-  res.render('cart.ejs',{cart:cart});
+  connection.execute(
+    'SELECT * FROM cart',
+    [],
+    (error, results) => {
+      res.render('cart.ejs', { cart: results });
+    } 
+  )
 });
 
-function addtocart(omusubi){
-  for (let i = 0;i<10;i++){
-    if (omusubi === cart[i][0]){
-      if (omusubilist[i][3]>0){
-        cart[i][2] += 1;
-        omusubilist[i][3]-=1; 
-      }
-    }
-  }
-}
+// function addtocart(omusubi){
+//   for (let i = 0;i<10;i++){
+//     if (omusubi === cart[i][0]){
+//       if (omusubilist[i][3]>0){
+//         cart[i][2] += 1;
+//         omusubilist[i][3]-=1; 
+//       }
+//     }
+//   }
+// }
 
-function deletefromCart(omusubi) {
-  for (let i = 0;i<10;i++){
-    if (omusubi === cart[i][0]){
-      if (cart[i][2] > 0){
-        cart[i][2] -= 1;
-        omusubilist[i][3]+=1;         
-      }
-    }
-  }
-}
+// function deletefromCart(omusubi) {
+//   for (let i = 0;i<10;i++){
+//     if (omusubi === cart[i][0]){
+//       if (cart[i][2] > 0){
+//         cart[i][2] -= 1;
+//         omusubilist[i][3]+=1;         
+//       }
+//     }
+//   }
+// }
 
-app.get('/cart', (req, res) => {
-  res.render('cart.ejs');
-});
+// app.get('/cart', (req, res) => {
+//   res.render('cart.ejs');
+// });
 
 app.post('/list', (req, res) => {
   const omusubi = req.body.buyingOmusubi;
-  addtocart(omusubi);
+  connection.query(
+    'UPDATE omusubilist SET stock = stock-1 WHERE name = ?',
+    [omusubi],
+    (error, results) => {
+    }
+  )
+  connection.query(
+    'UPDATE cart SET number = number+1 WHERE name = ?',
+    [omusubi],
+    (error, results) => {
+    }
+  );
   res.redirect('/list');
 });
 
 app.post('/cart', (req, res) => {
   const omusubi = req.body.deletingOmusubi;
-  deletefromCart(omusubi);
+  connection.query(
+    'SELECT  name = ?',
+    [omusubi],
+    (error, results) => {
+
+    }
+  );
+  connection.query(
+    'UPDATE cart SET number = 0 WHERE name = ?',
+    [omusubi],
+    (error, results) => {
+
+    }
+  );
   res.redirect('/cart');
 });
 
